a30bdd9a24dcaea180a5e21b47d182308b6ff8b2
CristianMor <Cristian.moreno@acuicultec.mx>
2023-02-02 11:44:35 -0700

    Creando vista cita

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
   @Override 
   public void start(Stage primaryState) throws Exception{
-    Parent root = FXMLLoader.load(new File("src/app/sample.fxml").toURI().toURL());
+    Parent root = FXMLLoader.load(new File("src/app/views/AppointmentView.fxml").toURI().toURL());
     primaryState.setTitle("Hola mundo");
     primaryState.setScene(new Scene(root, 300, 275));
     primaryState.show();
