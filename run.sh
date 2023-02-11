f3d4cb3a14e56126bf1668e53e330de21f59e5dd
CristianMor <Cristian.moreno@acuicultec.mx>
2023-02-10 23:14:43 -0700

    Sin poder enviar por correo

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-javac -d /Users/dev-mobile/Workspace/JavaProjects/SysCitas/bin --module-path /Users/dev-mobile/Downloads/openjfx-17/javafx-sdk-19.0.2.1/lib --add-modules javafx.controls,javafx.fxml -classpath /Users/dev-mobile/Workspace/JavaProjects/SysCitas/lib/json.jar ./src/app/Main.java ./src/app/controllers/Controller.java ./src/app/models/AppointmentModel.java ./src/app/models/classes/Appointment.java
-
-java -cp /Users/dev-mobile/Workspace/JavaProjects/SysCitas/lib/json.jar:bin --module-path /Users/dev-mobile/Downloads/openjfx-17/javafx-sdk-19.0.2.1/lib --add-modules javafx.controls,javafx.fxml app.Main
